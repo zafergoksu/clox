c918d1254f77ec94cabfcb97a4ac0e7e342077d2
zgoksu <zafergoksu00@gmail.com>
2022-07-07 23:16:55 +0100

    Add hash tables for ObjStrings to Values
    Using FNV-1a hashing algorithm to create hashes for strings and string interning to utilise the fact that two strings that are lexicographically the same should have the same hash code and be reused.

--- src/vm.c
+++ src/vm.c
@@ -146,9 +146,11 @@ static void runtimeError(const char* format, ...) {
 void initVM() {
     resetStack();
     vm.objects = NULL;
+    initTable(&vm.strings);
 }
 
 void freeVM() {
+    freeTable(&vm.strings);
     freeObjects();
 }
 
